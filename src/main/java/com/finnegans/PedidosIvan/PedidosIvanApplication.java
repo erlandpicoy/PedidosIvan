615a8a2719d7775c158d07cb919d051f07fc0f78
erlandpicoy <picoy.ivan@gmail.com>
2022-03-18 21:44:37 -0300

    Primer commit de Project

---
+++ src/main/java/com/finnegans/PedidosIvan/PedidosIvanApplication.java
@@ -0,0 +1,13 @@
+package com.finnegans.PedidosIvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosIvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosIvanApplication.class, args);
+	}
+
+}
